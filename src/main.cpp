a67c73a890b8eb07e721fdbb888107b2666abb32
Cryptic-Sailor <cryptic@example.com>
2022-10-26 16:51:26 -0500

    Trying to fix some bugs

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,10 @@
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
 //#include <NeoPixelBus.h>
-#include <NeoPixelAnimator.h>
+//#include <NeoPixelAnimator.h>
 #include <NeoPixelBrightnessBus.h>
 #include <ESP8266WebServer.h>
 #include <ArduinoJson.h>
-
 
 // Global Variables
 const uint8_t panelWidth = 32;
@@ -15,8 +14,8 @@ const uint8_t panelHeight = 8;
 const uint16_t pixelCount = panelWidth * panelHeight;
 const uint8_t brightness = 128;
 bool clear = false;
+bool github = true;
 ESP8266WebServer server(80);
-
 
 int cords[256][5];
 
@@ -26,20 +25,24 @@ NeoTopology<ColumnMajorAlternatingLayout> topo(panelWidth, panelHeight);
 
 NeoPixelBrightnessBus<NeoGrbFeature, Neo800KbpsMethod> strip(pixelCount);
 
+
 // RgbColor green(0, 128, 0);
 // RgbColor blue(0, 0, 128);
 RgbColor black(0);
 
-void handlePost(){
-  if (server.hasArg("plain") == false) {
-    //handle error here
+void handlePost()
+{
+  if (server.hasArg("plain") == false)
+  {
+    // handle error here
     server.send(200, "text/plain", "Body not received");
     return;
   }
-  //Serial.println("POST");
+  // Serial.println("POST");
   String body = server.arg("plain");
   Serial.println(body);
-  DynamicJsonDocument doc(24620);
+  DynamicJsonDocument doc(24700);
+  doc.clear();
   DeserializationError error = deserializeJson(doc, body);
   if (error)
   {
@@ -48,8 +51,8 @@ void handlePost(){
     return;
   }
   copyArray(doc["cords"], cords);
-  cordsCount = (int) doc["count"];
-  clear = (bool) doc["clear"];
+  cordsCount = (int)doc["count"];
+  clear = (bool)doc["clear"];
   doc.clear();
 
   server.send(200, "application/json", "{ \"status\": 0 }");
@@ -57,15 +60,15 @@ void handlePost(){
 
 void configureWebServer()
 {
-  server.on("/led", HTTP_POST, handlePost);	
-
+  server.on("/led", HTTP_POST, handlePost);
+  // Add handler for switching modes here
 }
 
 void setup()
 {
   WiFi.softAP("esp8266");
   Serial.begin(115200);
-  
+
   server.begin();
   configureWebServer();
 
@@ -74,7 +77,6 @@ void setup()
   strip.ClearTo(black);
   strip.Show();
   strip.SetBrightness(brightness);
-
 }
 
 void loop()
@@ -87,32 +89,31 @@ void loop()
     strip.Show();
     clear = false;
   }
-
-  if (cordsCount > 256)
+  if (github)
   {
-     cordsCount = 0;
-  }
+    // if (cordsCount > 257)
+    // {
+    //   cordsCount = 0;
+    // }
 
-  if (cordsCount > 0){
-    for ( int i = 0; i < cordsCount; i++)
+    if (cordsCount > 0)
     {
-      // Serial.print(cords[i][0]);
-      // Serial.print(" ,");
-      // Serial.println(cords[i][1]);
-      // Serial.print(" ,");
-      // Serial.println(cords[i][2]);
-      // Serial.print(" ,");
-      // Serial.println(cords[i][3]);
-      // Serial.print(" ,");
-      // Serial.println(cords[i][4]);
-      
-      strip.SetPixelColor(topo.Map(cords[i][0], cords[i][1]), RgbColor(cords[i][2], cords[i][3], cords[i][4]));
+      for (int i = 0; i < cordsCount; i++)
+      {
+        // Serial.print(cords[i][0]);
+        // Serial.print(" ,");
+        // Serial.println(cords[i][1]);
+        // Serial.print(" ,");
+        // Serial.println(cords[i][2]);
+        // Serial.print(" ,");
+        // Serial.println(cords[i][3]);
+        // Serial.print(" ,");
+        // Serial.println(cords[i][4]);
+
+        strip.SetPixelColor(topo.Map(cords[i][0], cords[i][1]), RgbColor(cords[i][2], cords[i][3], cords[i][4]));
+      }
+      cordsCount = 0;
+      strip.Show();
     }
-  cordsCount = 0;
-  strip.Show();
   }
-
-
-
 }
-
